6b8f6c5f327c30d80148c8fd99ed322100e364db
Anton <110679298+Anton162@users.noreply.github.com>
2023-09-24 18:10:46 +0300

    test 1.0

---
+++ src/components/Profiler.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" />
+      </div>
+
+      <div>avatar discription</div>
+      <div>
+        My posts
+        <div>
+          New Post
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
